69588c88934ac6f927023dd46cc108c80aa1f709
Esteban Echeverry <eecheverry@knowark.com>
2021-12-27 17:28:34 +0000

    Check datastore.

--- tests/repository/memory.spec.js
+++ tests/repository/memory.spec.js
@@ -23,8 +23,8 @@ describe('MemoryRepository', () => {
 
   it('adds an entity to its data store', async () => {
     const id = uuid()
-    const items = [new Entity({ id: id, name: 'John Doe' })]
-    const records = await repository.add(items)
+    const item = new Entity({ id: id, name: 'John Doe' })
+    const records = await repository.add(item)
 
     const [record] = records
 
@@ -34,6 +34,23 @@ describe('MemoryRepository', () => {
     expect(record.createdAt).toBe(mockTimestamp)
     expect(record.updatedAt).toBe(mockTimestamp)
     expect(repository.data.default[id]).toBe(record)
+  })
+
+  it('adds multiple entities to its data store', async () => {
+    const id1 = uuid()
+    const id2 = uuid()
+    const items = [
+      new Entity({ id: id1, name: 'John Doe' }),
+      new Entity({ id: id2, nmae: 'Richard Roe' })
+    ]
+    const records = await repository.add(items)
+
+    const [record1, record2] = records
+
+    expect(Array.isArray(records)).toBe(true)
+    expect(records.length).toBe(2)
+    expect(repository.data.default[id1]).toBe(record1)
+    expect(repository.data.default[id2]).toBe(record2)
   })
 
   it('removes an entity from its data store', async () => {
