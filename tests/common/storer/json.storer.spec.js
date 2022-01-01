7c6732d7420da96c0d45bc50586efa0ef86c40e3
Esteban Echeverry <eecheverry@knowark.com>
2022-01-01 19:33:01 +0000

    Implement JsonStorer retrieve method.

---
+++ tests/common/storer/json.storer.spec.js
@@ -0,0 +1,68 @@
+import * as os from 'os'
+import * as path from 'path'
+import * as fs from 'fs/promises'
+import { describe, expect, it, beforeEach, afterEach } from '@jest/globals'
+import { JsonStorer } from '../../../lib/common/storer'
+
+describe('JsonStorer', () => {
+  let storer = /** @type {MemoryStorer} */ (null)
+  let directory = null
+
+  beforeEach(async () => {
+    directory = await fs.mkdtemp(path.join(os.tmpdir(), 'data'))
+    const collection = 'orders'
+    storer = new JsonStorer({ directory, collection })
+  })
+
+  afterEach(async () => {
+    await fs.rmdir(directory, { recursive: true })
+  })
+
+  it('can be instantiated', () => {
+    expect(storer).toBeTruthy()
+  })
+
+  it('holds the given collection string', () => {
+    expect(storer.directory).toBe(directory)
+    expect(storer.collection).toBe('orders')
+  })
+
+  it('defines a "retrieve" method that returns the keyed store', async () => {
+    const itemId = 'c0cbea06-2f4b-4bf8-a508-8fe9725cb1cf'
+    const storeId = '4d68cd64-d4fd-4d36-adf8-8efefd226a9a'
+    const data = {
+      [itemId]: { id: itemId, name: 'John Doe' }
+    }
+
+    const file = path.join(directory, storeId, storer.collection)
+    await fs.mkdir(path.parse(file).dir, { recursive: true })
+    await fs.writeFile(`${file}.json`, JSON.stringify(data))
+
+    const store = await storer.retrieve(storeId)
+
+    expect(store).toMatchObject(data)
+  })
+
+  it('retrieves an empty object if the file is not found', async () => {
+    const storeId = '4d68cd64-d4fd-4d36-adf8-8efefd226a9a'
+
+    const store = await storer.retrieve(storeId)
+
+    expect(store).toMatchObject({})
+  })
+
+  // it('defines an "store" method', async () => {
+  // const itemId = 'ac283465-2fec-4902-84dd-d497af0aae04'
+  // const data = {
+  // [itemId]: {
+  // id: itemId,
+  // name: 'John Doe'
+  // }
+  // }
+  // const storeId = '4d68cd64-d4fd-4d36-adf8-8efefd226a9a'
+
+  // await storer.store(storeId, data)
+
+  // expect(storer.data[storeId]).toBe(data)
+  // })
+})
