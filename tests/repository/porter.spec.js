fdfcda648842a91cc3e9a891e5fe945ca6008cdc
Esteban Echeverry <eecheverry@knowark.com>
2022-02-07 21:29:56 +0000

    Rename persiter to porter.

--- tests/repository/porter.spec.js
+++ tests/repository/porter.spec.js
@@ -1,27 +1,27 @@
 import { describe, expect, it, beforeEach } from '@jest/globals'
 import { Entity } from '../../lib/common/entity.js'
-import { Persister, MemoryRepository } from '../../lib/repository'
+import { Porter, MemoryRepository } from '../../lib/repository'
 
 class Alpha extends Entity {}
 
 class Beta extends Entity {}
 
-describe('Persister', () => {
-  let persister = null
+describe('Porter', () => {
+  let porter = null
 
   beforeEach(function () {
     const alphaRepository = new MemoryRepository({ model: Alpha })
-    persister = new Persister({
+    porter = new Porter({
       repositories: [alphaRepository]
     })
   })
 
   it('is defined', function () {
-    expect(persister).toBeTruthy()
+    expect(porter).toBeTruthy()
   })
 
   it('gets the repository associated to the given model', () => {
-    const repository = persister.get(Alpha)
+    const repository = porter.get(Alpha)
 
     expect(repository.model).toBe(Alpha)
   })
@@ -29,9 +29,9 @@ describe('Persister', () => {
   it('sets the repository associated to the given model', () => {
     const betaRepository = new MemoryRepository({ model: Beta })
 
-    persister.put(betaRepository)
+    porter.put(betaRepository)
 
-    const repository = persister.get(Beta)
+    const repository = porter.get(Beta)
     expect(repository.model).toBe(Beta)
     expect(repository).toBe(betaRepository)
   })
