5611cea194699869a656db5f21bdf6a84025d53d
Esteban Echeverry <eecheverry@knowark.com>
2022-03-14 23:23:08 +0000

    Cover find method.

--- tests/repository/repository.spec.js
+++ tests/repository/repository.spec.js
@@ -39,7 +39,7 @@ describe('Repository', () => {
     expect(typeof method).toBe('function')
   })
 
-  it('finds entities based on a list of scalars', async () => {
+  it('finds entities based on a list of strings', async () => {
     const searchResult = [
       new ConcreteEntity({ id: 'C001' }),
       new ConcreteEntity({ id: 'C002' }),
@@ -54,5 +54,74 @@ describe('Repository', () => {
     expect(concreteRepository._searchArguments).toEqual([
       [['id', 'in', ['C002', 'C003']]], { limit: null, offset: null }
     ])
+  })
+
+  it('finds entities based on a list of objects', async () => {
+    const searchResult = [
+      new ConcreteEntity({ id: 'C001' }),
+      new ConcreteEntity({ id: 'C002' }),
+      new ConcreteEntity({ id: 'C003' })
+    ]
+    const concreteRepository = new ConcreteRepository({ searchResult })
+    const records = [{ id: 'C002' }, { id: 'C003' }]
+
+    const found = await concreteRepository.find(records)
+
+    expect(found).toEqual(searchResult.slice(1))
+    expect(concreteRepository._searchArguments).toEqual([
+      [['id', 'in', ['C002', 'C003']]], { limit: null, offset: null }
+    ])
+  })
+
+  it('finds entities based on a string', async () => {
+    const searchResult = [
+      new ConcreteEntity({ id: 'C001' }),
+      new ConcreteEntity({ id: 'C002' }),
+      new ConcreteEntity({ id: 'C003' })
+    ]
+    const concreteRepository = new ConcreteRepository({ searchResult })
+    const records = 'C003'
+
+    const found = await concreteRepository.find(records)
+
+    expect(found).toEqual(searchResult.slice(2))
+    expect(concreteRepository._searchArguments).toEqual([
+      [['id', 'in', ['C003']]], { limit: null, offset: null }
+    ])
+  })
+
+  it('returns null if entities not found', async () => {
+    const searchResult = [
+      new ConcreteEntity({ id: 'C001' }),
+      new ConcreteEntity({ id: 'C002' }),
+      new ConcreteEntity({ id: 'C003' })
+    ]
+    const concreteRepository = new ConcreteRepository({ searchResult })
+    const records = ['C002', 'C003', 'C004']
+
+    const found = await concreteRepository.find(records)
+
+    expect(found).toEqual([searchResult[1], searchResult[2], null])
+    expect(concreteRepository._searchArguments).toEqual([
+      [['id', 'in', ['C002', 'C003', 'C004']]], { limit: null, offset: null }
+    ])
+  })
+
+  it('can initialize entities if not found', async () => {
+    const model = ConcreteEntity
+    const searchResult = [
+      new ConcreteEntity({ id: 'C001' }),
+      new ConcreteEntity({ id: 'C002' }),
+      new ConcreteEntity({ id: 'C003' })
+    ]
+    const concreteRepository = new ConcreteRepository(
+      { model, searchResult })
+    const records = [{ id: 'C002' }, { id: 'C003' }, { id: 'C004' }]
+
+    const found = await concreteRepository.find(records, { init: true })
+
+    expect(found[0].id).toEqual('C002')
+    expect(found[1].id).toEqual('C003')
+    expect(found[2].id).toEqual('C004')
   })
 })
