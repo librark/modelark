bc5382a2adfca98322bad0f260039966aa8cdbba
Esteban Echeverry <eecheverry@knowark.com>
2022-04-11 17:36:58 +0100

    Update JsonRepository constructor parameter.

--- tests/repository/json.repository.spec.js
+++ tests/repository/json.repository.spec.js
@@ -6,11 +6,9 @@ import { JsonRepository } from '../../lib/repository'
 
 describe('JsonRepository', () => {
   let repository = null
-  const mockTimestamp = 1640446104
 
   beforeEach(function () {
-    const mockDate = { now: () => mockTimestamp * 1000 }
-    repository = new JsonRepository({ clock: mockDate })
+    repository = new JsonRepository()
   })
 
   it('is defined', function () {
@@ -22,6 +20,18 @@ describe('JsonRepository', () => {
     expect(repository.locator instanceof DefaultLocator).toBe(true)
     expect(repository.filterer instanceof Filterer).toBe(true)
     expect(repository.storer instanceof JsonStorer).toBe(true)
-    expect(repository.clock).toBe(Date)
+  })
+
+  it('is defined with custom location and store arguments', function () {
+    const repository = new JsonRepository({
+      directory: '/tmp/custom/directory',
+      collection: 'elements',
+      reference: 'editor',
+      location: 'namespace'
+    })
+    expect(repository.locator.location()).toBe('namespace')
+    expect(repository.locator.reference()).toBe('editor')
+    expect(repository.storer.directory).toBe('/tmp/custom/directory')
+    expect(repository.storer.collection).toBe('elements')
   })
 })
