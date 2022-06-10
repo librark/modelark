145863d70f9c65f21d5008b7e36cd49d161f8bba
Esteban Echeverry <eecheverry@knowark.com>
2022-06-10 08:05:24 +0100

    Implement SqlRepository add method.

---
+++ tests/common/format.spec.js
@@ -0,0 +1,31 @@
+import { describe, expect, it } from '@jest/globals'
+import { camelToSnake, snakeToCamel, dedent } from '../../lib/common/format.js'
+
+describe('Format', () => {
+  it('can convert snake to camel', () => {
+    const kebab = 'my_unique_variable'
+    const camel = snakeToCamel(kebab)
+    expect(camel).toBe('myUniqueVariable')
+  })
+
+  it('can convert camel to snake', () => {
+    const camel = 'myUniqueVariable'
+    const kebab = camelToSnake(camel)
+    expect(kebab).toBe('my_unique_variable')
+  })
+
+  it('can dedent a certain text', () => {
+    const text = `Hi,
+    this text has a nice indentation
+    because we defined a dedent function.`
+
+    const expected = (
+      'Hi,\n' +
+      'this text has a nice indentation\n' +
+      'because we defined a dedent function.')
+
+    const result = dedent(text)
+
+    expect(result).toBe(expected)
+  })
+})
---
+++ tests/repository/sql.repository.spec.js
@@ -0,0 +1,95 @@
+import { describe, expect, it, beforeEach } from '@jest/globals'
+import {
+  uuid, dedent, DefaultLocator, Entity
+} from '../../lib/common/index.js'
+import { Connector, Connection } from '../../lib/connector/index.js'
+import { SqlRepository } from '../../lib/repository'
+
+class CustomEntity extends Entity {
+  constructor (attributes) {
+    super(attributes)
+    this.name = attributes.name || ''
+  }
+}
+
+class MockConnection extends Connection {
+  constructor () {
+    super()
+    this.statements = []
+    this.parameters = []
+  }
+
+  async query (statement, parameters) {
+    this.statements.push(statement)
+    this.parameters.push(parameters)
+  }
+}
+
+class MockConnector extends Connector {
+  constructor () {
+    super()
+    this.connections = []
+  }
+
+  async get () {
+    const connection = new MockConnection()
+    this.connections.push(connection)
+    return connection
+  }
+}
+
+describe('SqlRepository', () => {
+  let repository = null
+  const mockTimestamp = 1654460843
+
+  beforeEach(function () {
+    const model = CustomEntity
+    const locator = new DefaultLocator(
+      { reference: 'editor', location: 'namespace' })
+    const connector = new MockConnector()
+    const table = 'elements'
+    const clock = { now: () => mockTimestamp * 1000 }
+
+    repository = new SqlRepository({
+      model, locator, connector, table, clock
+    })
+  })
+
+  it('is defined', function () {
+    expect(repository).toBeTruthy()
+  })
+
+  it('is defined with custom location and store arguments', function () {
+    expect(repository.model).toBe(CustomEntity)
+    expect(repository.locator.location()).toBe('namespace')
+    expect(repository.locator.reference()).toBe('editor')
+  })
+
+  it('adds an entity to its data store', async () => {
+    const id = 'C001'
+    const item = new CustomEntity({ id: id, name: 'John Doe' })
+
+    const records = await repository.add(item)
+
+    const [record] = records
+    const [connection] = repository.connector.connections
+    expect(Array.isArray(records)).toBe(true)
+    expect(records.length).toBe(1)
+    expect(record.id).toEqual(id)
+    expect(repository.connector.connections.length).toEqual(1)
+    expect(dedent(connection.statements[0]).trim()).toEqual(
+      dedent('INSERT INTO elements (id, status, created_at, ' +
+        'updated_at, created_by, updated_by, name) VALUES\n' +
+        '($1, $2, $3, $4, $5, $6, $7)\n' +
+        'ON CONFLICT (id) DO UPDATE SET\n' +
+        'status=excluded.status, created_at=excluded.created_at, ' +
+        'updated_at=excluded.updated_at, created_by=excluded.created_by, ' +
+        'updated_by=excluded.updated_by, name=excluded.name\n' +
+        'RETURNING *;\n'
+      ).trim())
+    expect(connection.parameters[0]).toEqual([
+      'C001', '', 1656116959, 1656116959,
+      'editor', 'editor', 'John Doe'
+    ])
+  })
+})
